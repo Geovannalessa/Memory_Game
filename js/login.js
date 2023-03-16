18ce9534ce68c0d5084644a442c056bd025bb9cb
Geovanna Lessa <geovannalessa2@gmail.com>
2023-03-16 19:41:02 -0300

    Página de login Criada e o salvando o player com localStorage

---
+++ js/login.js
@@ -0,0 +1,20 @@
+const input = document.querySelector('.login_input');
+const button_login = document.querySelector(".login_button");
+const form = document.querySelector('.login-form');
+
+const validateInput = ({ target }) =>{
+    if( target.value.length > 2 ){
+        button_login.removeAttribute('disabled');
+        return;
+    } 
+    button_login.setAttribute('disabled', '');
+} 
+
+const handleSubmit = (event) =>{
+    event.preventDefault();
+    localStorage.setItem('player', input.value);
+    window.location = 'pages/game.html'
+}
+input.addEventListener("input", validateInput);
+form.addEventListener("submit", handleSubmit);
+
